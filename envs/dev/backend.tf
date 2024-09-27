5e2281ad376301e07970c97ae55d4e005256d7fb
MD. Osman Gani Khan Masum <ogkmasum@gmail.com>
2024-09-28 01:06:35 +0600

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-state-bucket"
+        bucket = "l002-terraform-state-bucket"
         key = "dev/terraform.tfstate"
-        region = "us-east-1"
+        region = "us-east-2"
     }
 }
