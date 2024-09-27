b4bb8411515352cecfa1e6c1668080c7f3338c74
MD. Osman Gani Khan Masum <ogkmasum@gmail.com>
2024-09-28 01:07:02 +0600

    instance count added

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,13 +1,17 @@
-module "vpc" {
-    source = "../../modules/vpc"
+provider "aws" {
+  region = "us-east-2"
+}
 
-    cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source     = "../../modules/vpc"
+  cidr_block = "10.0.0.0/16"
 }
 
 module "ec2" {
-    source = "../../modules/ec2"
+  source      = "../../modules/ec2"
+  instance_type = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+  subnet_id     = module.vpc.public_subnet_id
+  instance_count = 2  
 
-    instance_type = "t2.micro"
-    ami = "ami-0c55b159cbfafe1f0"
-    vpc_id = module.vpc.vpc_id
 }
