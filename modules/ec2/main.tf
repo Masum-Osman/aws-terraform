1f904b3fbb0d3f9be6129648fe9296d30ed009ce
MD. Osman Gani Khan Masum <ogkmasum@gmail.com>
2024-09-28 01:09:05 +0600

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,25 +1,35 @@
 variable "instance_type" {
-    description = "The type of instance to use"
+  description = "The type of instance to use"
+  type        = string
 }
 
 variable "ami" {
-    description = "The AMI ID to use"
+  description = "The AMI ID to use"
+  type        = string
 }
 
-variable "vpc_id" {
-    description = "The VPC ID to launch the instance in"
+variable "subnet_id" {
+  description = "The Subnet ID to launch the instance in"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "The number of instances to create"
+  type        = number
+  default     = 1
 }
 
 resource "aws_instance" "this" {
-    ami = var.ami
-    instance_type = var.instance_type
-    subnet_id = var.vpc_id
+  count         = var.instance_count
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
 
-    tags = {
-        Name = "TerraformInstance"
-    }
+  tags = {
+    Name = "TerraformInstance-${count.index}"
+  }
 }
 
 output "instance_id" {
-    value = aws_instance.this.id
+  value = aws_instance.this[*].id
 }
