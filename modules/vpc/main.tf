8514bb7d4d63a9b912a4494c3ac0eb9c1e2a3bdd
MD. Osman Gani Khan Masum <ogkmasum@gmail.com>
2024-09-28 01:09:29 +0600

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,11 +1,17 @@
-variable "cidr_block" {
-    description = "The CIDR block for the VPC"
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
 }
 
-resource "aws_vpc" "this" {
-    cidr_block = var.cidr_block
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
 }
 
 output "vpc_id" {
-    value = aws_vpc.this.id
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
 }
