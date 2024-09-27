c645e9be754ef62d1905fe541e2a32dc20e01b26
MD. Osman Gani Khan Masum <ogkmasum@gmail.com>
2024-09-28 01:07:44 +0600

    Update providers.tf

--- global/providers.tf
+++ global/providers.tf
@@ -1,8 +1,21 @@
-provider "aws" {
-    region = var.region
+# provider "aws" {
+#     region = var.region
+# }
+
+# variable "region" {
+#   description = "The AWS region to deploy resources in"
+#   default     = "us-east-2"
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Use a specific version as needed
+    }
+  }
 }
 
-variable "region" {
-  description = "The AWS region to deploy resources in"
-  default     = "us-east-1"
+provider "aws" {
+  region = var.region
 }
